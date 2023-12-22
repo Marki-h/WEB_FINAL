08476d49db464e40cb7fb3fcaf1cafe56e6075fa
Marki-h <131808544+Marki-h@users.noreply.github.com>
2023-12-22 11:55:40 -0500

    Add files via upload

--- recettes.js
+++ recettes.js
@@ -1,5 +1,3 @@
-let nombre_recettes = 0
-
 // Pour tout afficher :
 function tout_afficher(){
     $.getJSON("https://6585a1cc022766bcb8c90b5d.mockapi.io/recettes")
@@ -8,7 +6,7 @@ function tout_afficher(){
             Nouv_Recette(recette)
         })
     })
-    .fail(function (erreur){
+    .fail(function (){
         alert("Le fichier jSON n'a pas bien fonctionné `(")
     })
 }
@@ -29,15 +27,31 @@ function Recette(nom = "", ingredient1 ="",  ingredient2 ="", ingredient3 ="", i
     this.quantite5 = quantite5;
     this.instructions = description;
 }
+
+// Fonction pour créer la nouvelle recette a partir du jSON de mokapi
 function Nouv_Recette(){
     const  nouv_recette = new Recette();
-    let valide = true;
+    let recette = nouv_recette.nom,
+        ing1 = nouv_recette.ingredient1,
+        ing2 = nouv_recette.ingredient2,
+        ing3 = nouv_recette.ingredient3,
+        ing4 = nouv_recette.ingredient4,
+        ing5 = nouv_recette.ingredient5,
+        q1 = nouv_recette.quantite1,
+        q2 = nouv_recette.quantite2,
+        q3 = nouv_recette.quantite3,
+        q4 = nouv_recette.quantite4,
+        q5 = nouv_recette.quantite5,
+        desc = nouv_recette.instructions
+    // AFficher la recette dans la liste des recettes de la page
+    $("#list_recettes").append("<li class='ms-2 align-items-center'><strong>"+recette+"</strong><br>Ingrédients :<br><ul><li>"+q1+', '+ing1+"</li><li>"+q2+', '+ing2+"</li><li>"+q3+', '+ing3+"</li><li>"+q4+', '+ing4+"</li><li>"+q5+', '+ing5+"</li></ul><br>Instructions : <br><p>"+desc+"</p><input type='submit' value='Supprimer' class='btn btn-danger mb-3' id='Supprimer'></li>").css("border","solid grey 2px").css("margin-bottom","2px").css("width","300px").css("margin-top","5px")
+
+
 
 }
 // Pour afficher chaque recette :
 $("#submit").click(function(){
-    let valide = true,
-        recette = $('#nom').val(),
+    let recette = $('#nom').val(),
         ing1 = $('#ingredient1').val(),
         ing2 = $('#ingredient2').val(),
         ing3 = $('#ingredient3').val(),
@@ -54,7 +68,18 @@ $("#submit").click(function(){
         alert = ("Certains ou tous les champs sont vides !")
     }
     else{
-        $("#list_recettes").append("<li><strong>Nom : </strong>"+recette+"<br><strong>Ingrédients :</strong><br><ul><li>"+q1+', '+ing1+"</li><li>"+q2+', '+ing2+"</li><li>"+q3+', '+ing3+"</li><li>"+q4+', '+ing4+"</li><li>"+q5+', '+ing5+"</li></ul><br><strong>Instructions : </strong><br><p>"+desc+"</p><input type='submit' value='Supprimer' class='btn btn-danger' id='Supprimer'></li>").css("border","solid grey 2px").css("margin-bottom","2px").css("width","375px").css("margin-top","5px")
+        $("#list_recettes").append("<li class='ms-2 align-items-center'><strong>"+recette+"</strong><br>Ingrédients :<br><ul><li>"+q1+', '+ing1+"</li><li>"+q2+', '+ing2+"</li><li>"+q3+', '+ing3+"</li><li>"+q4+', '+ing4+"</li><li>"+q5+', '+ing5+"</li></ul><br>Instructions : <br><p>"+desc+"</p><input type='submit' value='Supprimer' class='btn btn-danger mb-3' id='Supprimer'></li>").css("border","solid grey 2px").css("margin-bottom","2px").css("width","300px").css("margin-top","5px")
+        localStorage.setItem("recette",recette)
     }
 
 })
+
+// Fonction pour verifier si l'utilisateur a le droit de supprimer
+$('#Supprimer').click(function(){
+    // Trouver le li sur lequel l'utilisateur clique
+    // AUCUNE IDEE COMMENT FAIRE xD
+    // Trouver la clé dans le stockage local
+    if(localStorage.getItem("recette", "Nom de la clé"){
+        delete // le li en question
+    }
+}
